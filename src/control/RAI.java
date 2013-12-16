072b198eda98a7678ddb725174dba9ebb99559da
groenborg <groenborg27@gmail.com>
2013-12-16 10:39:02 +0100

    Flow 5

---
+++ src/control/RAI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+/**
+ *
+ * @author Simon og Kasper
+ */
+public class RAI {
+
+    /**
+     * @param args the command line arguments
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
