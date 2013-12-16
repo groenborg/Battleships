73b6004edb8f5479fccb0e3c3300428c566e47f1
groenborg <groenborg27@gmail.com>
2013-12-16 10:44:19 +0100

    Version 0.0.0.1

--- src/control/RAI.java
+++ src/control/RAI.java
@@ -1,20 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package control;
 
 /**
  *
  * @author Simon og Kasper
  */
+
+
 public class RAI {
 
-    /**
-     * @param args the command line arguments
-     * 
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    
+    
+    
 }
