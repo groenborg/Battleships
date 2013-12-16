36da18a2b87ae3cf937df9905050a1e06f261618
groenborg <groenborg27@gmail.com>
2013-12-16 13:10:48 +0100

    version 0.0.2.0

--- src/captainobvious/RAI.java
+++ src/captainobvious/RAI.java
@@ -1,33 +1,28 @@
 package captainobvious;
+
 import battleships.BattleshipAI;
 import battleships.Board;
 import battleships.Fleet;
 import battleships.Position;
+
 /**
  *
  * @author Simon og Kasper
  */
+public class RAI implements BattleshipAI {
 
-
-
-public class RAI implements BattleshipAI{
-
-    
     private final String aiName = "Captain Obvious";
-    private Position current; 
+    private Position current;
+    private Field[][] map;
+    private int matchNumber;
+    private int sizeX = 10;
+    private int sizeY = 10;
     
-    private int matchNumber; 
-    
-    
-    public RAI(){
-    
-    
+
+    public RAI() {
+        this.map = constructMap();
     }
 
-    
-    
-    
-    
     @Override
     public String getName() {
         return aiName;
@@ -35,27 +30,49 @@ public class RAI implements BattleshipAI{
 
     @Override
     public void newMatch(int i) {
-        
     }
 
     @Override
     public void placeShips(Fleet fleet, Board board) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+
+
+
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void incoming(Position pstn) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public Position getFireCoordinates(Fleet fleet) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void hitFeedBack(boolean bln, Fleet fleet) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
+    // PRivate methods here
+    private Field[][] constructMap() {
+        Field[][] mapField = new Field[sizeX][sizeY];
+        for (int x = 0; x < this.sizeX; ++x) {
+            for (int y = 0; y < this.sizeY; ++y) {
+                mapField[x][y] = new Field();
+            }
+        }
+        return mapField;
+    }
+
+    private void resetMapFields() {
+        for (int x = 0; x < this.sizeX;) {
+            for (int y = 0; y < this.sizeY; ++y) {
+                
+            }
+        }
+
+
+    }
 }
