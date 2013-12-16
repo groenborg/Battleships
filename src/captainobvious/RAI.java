9ef9e10481f8d3cb2278f79b568a5d684029b3ee
groenborg <groenborg27@gmail.com>
2013-12-16 14:32:28 +0100

    CO 6

--- src/captainobvious/RAI.java
+++ src/captainobvious/RAI.java
@@ -4,6 +4,8 @@ import battleships.BattleshipAI;
 import battleships.Board;
 import battleships.Fleet;
 import battleships.Position;
+import battleships.Ship;
+import java.util.Random;
 
 /**
  *
@@ -14,11 +16,15 @@ public class RAI implements BattleshipAI {
     private final String aiName = "Captain Obvious";
     private Position current;
     private Field[][] map;
+    private Random rnd;
     private int matchNumber;
     private int sizeX = 10;
     private int sizeY = 10;
+    private int nextX;
+    private int nextY;
 
     public RAI() {
+        this.rnd = new Random();
         this.map = constructMap();
     }
 
@@ -34,17 +40,45 @@ public class RAI implements BattleshipAI {
 
     @Override
     public void placeShips(Fleet fleet, Board board) {
+
         resetMapFields(false);
+        nextX = 0;
+        nextY = 0;
+        sizeX = board.sizeX();
+        sizeY = board.sizeY();
 
-
-
-
+        for (int i = 0; i < fleet.getNumberOfShips(); ++i) {
+            Ship s = fleet.getShip(i);
+            boolean vertical = rnd.nextBoolean();
+            boolean finished = true;
+            Position pos = new Position(1, 1);
+            if (vertical) {
+                while (!finished) {
+                    int x = rnd.nextInt(sizeX);
+                    int y = rnd.nextInt(sizeY - (s.size() - 1));
+                    if (!checkField(x, y, s, vertical)) {
+                        pos = new Position(x, y);
+                        prodShip(x, y, s, vertical);
+                        finished = true;
+                    }
+                }
+            } else {
+                while (!finished) {
+                    int x = rnd.nextInt(sizeX - (s.size() - 1));
+                    int y = rnd.nextInt(sizeY);
+                    if (!checkField(x, y, s, vertical)) {
+                        pos = new Position(x, y);
+                        prodShip(x, y, s, vertical);
+                        finished = true;
+                    }
+                }
+            }
+            board.placeShip(pos, s, vertical);
+        }
     }
 
     @Override
     public void incoming(Position pstn) {
-        
-        
     }
 
     @Override
@@ -58,6 +92,32 @@ public class RAI implements BattleshipAI {
     }
 
     // Private methods here
+    private void prodShip(int x, int y, Ship s, boolean vertical) {
+        for (int a = 0; a < s.size(); ++a) {
+            this.map[x][y].setUsShip(true);
+            if (vertical) {
+                y++;
+            } else {
+                x++;
+            }
+        }
+
+    }
+
+    private boolean checkField(int x, int y, Ship s, boolean vertical) {
+        for (int a = 0; a < s.size(); ++a) {
+            if (this.map[x][y].getUsShip()) {
+                return true;
+            }
+            if (vertical) {
+                y++;
+            } else {
+                x++;
+            }
+        }
+        return false;
+    }
+
     private Field[][] constructMap() {
         Field[][] mapField = new Field[sizeX][sizeY];
         for (int x = 0; x < this.sizeX; ++x) {
@@ -71,9 +131,9 @@ public class RAI implements BattleshipAI {
     private void resetMapFields(boolean all) {
         for (int x = 0; x < this.sizeX;) {
             for (int y = 0; y < this.sizeY; ++y) {
-                if(all){
+                if (all) {
                     this.map[x][y].resetMatch();
-                }else{
+                } else {
                     this.map[x][y].resetRound();
                 }
             }
