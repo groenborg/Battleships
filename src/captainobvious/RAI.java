6c2720d6c2b501b727f0f55e9a72d3d64d336b58
groenborg <groenborg27@gmail.com>
2013-12-16 12:52:29 +0100

    version 0.0.1.0

--- src/captainobvious/RAI.java
+++ src/captainobvious/RAI.java
@@ -1,19 +1,61 @@
 package captainobvious;
-
+import battleships.BattleshipAI;
+import battleships.Board;
+import battleships.Fleet;
+import battleships.Position;
 /**
  *
  * @author Simon og Kasper
  */
 
 
-public class RAI {
 
+public class RAI implements BattleshipAI{
+
+    
+    private final String aiName = "Captain Obvious";
+    private Position current; 
+    
+    private int matchNumber; 
+    
     
     public RAI(){
     
     
     }
+
     
     
+    
+    
+    @Override
+    public String getName() {
+        return aiName;
+    }
+
+    @Override
+    public void newMatch(int i) {
+        
+    }
+
+    @Override
+    public void placeShips(Fleet fleet, Board board) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void incoming(Position pstn) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Position getFireCoordinates(Fleet fleet) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public void hitFeedBack(boolean bln, Fleet fleet) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
     
 }
